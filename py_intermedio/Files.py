9930f5aa6e9de44415b8a3fde5c19086a31c9867
Yeison Sanabria <yeison.sanabria@wiedii.co>
2022-08-05 15:04:45 -0500

    curso python intermedio finalizado

--- py_intermedio/Files.py
+++ py_intermedio/Files.py
@@ -1,19 +1,62 @@
-def read():
-    numbers = []
-    with open("py_intermedio/Files/numbers.txt","r", encoding="utf-8") as f:
-        for line in f:
-            numbers.append(int(line))
-    print(numbers) 
-           
-def writte():
-    names = ['ivan','jesus','sarai','cesar','andres','yeison','bruno']
-    with open("py_intermedio/Files/names.txt","a", encoding="utf-8") as f:
-        for name in names:
-            f.write(name)
-            f.write("\n")
+import os
+import random
+
+
+def read_words(filepath='py_intermedio/Files/names.txt'):
+
+    with open(filepath, 'r', encoding='utf-8') as f:
+        list_words = [word.strip().upper() for word in f] # el strip() quita ese espacio final
+        choosen_word = list_words[random.randint(0, len(list_words))]
+
+    return choosen_word
+
+
+def game(magic_word, letter, game_word):
+    if letter in magic_word:
+        for i in range(len(magic_word)):
+            if letter == magic_word[i]:
+                game_word[i] = letter
+
+    return ' '.join(game_word)
+
 
 def run():
-    writte()
+    lives = 5
+    letter = ''
+    magic_word = read_words()
+    game_word = ['_' for i in range(len(magic_word))]
+
+    while lives > 0:
+        os.system('cls')
+        print(f'Vidas restantes: {"❤" * (lives)}')
+        print('¡Adivina la palabra!')
+        print(game(magic_word, letter, game_word))
+        try:
+            if len(letter) > 1:
+                raise ValueError('Solo puedes ingresar una letra')
+            else:
+                if game(magic_word, letter, game_word).count('_') > 0:
+                    if letter in game(magic_word, letter, game_word):
+                        letter = input('Escoge una letra: ').upper()
+                    else:
+                        lives -= 1
+                        if lives > 0:
+                            os.system('cls')
+                            print(f'Vidas restantes: {"❤" * (lives)}')
+                            print('¡Adivina la palabra!')
+                            print(game(magic_word, letter, game_word))
+                            letter = input('Escoge una letra: ').upper()
+                        else:
+                            os.system('clear')
+                            print('¡Te quedaste sin vidas!\nJuego terminado...\n')
+                            print('La palabra era: ' + magic_word)
+                else:
+                    print('¡Ganaste! √')
+                    break
+
+        except ValueError as ve:
+            return print(ve)
+    
 
 if __name__ == '__main__':
     run()
