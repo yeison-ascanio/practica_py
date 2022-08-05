7e8764d345c34cb3854948231a3ec627e2038ec8
Yeison Sanabria <yeison.sanabria@wiedii.co>
2022-08-05 14:50:38 -0500

    clase 18  python intermedio

---
+++ py_intermedio/Files.py
@@ -0,0 +1,23 @@
+import numbers
+from turtle import numinput
+
+
+def read():
+    numbers = []
+    with open("py_intermedio/Files/numbers.txt","r", encoding="utf-8") as f:
+        for line in f:
+            numbers.append(int(line))
+    print(numbers) 
+           
+def writte():
+    names = ['ivan','jesus','sarai','cesar','andres','yeison','bruno']
+    with open("py_intermedio/Files/names.txt","a", encoding="utf-8") as f:
+        for name in names:
+            f.write(name)
+            f.write("\n")
+
+def run():
+    writte()
+
+if __name__ == '__main__':
+    run()
